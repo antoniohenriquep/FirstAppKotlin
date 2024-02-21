e3f77aca2c76312ef10891a5d4b91514b23ce8ed
antoniohenriquep <antoniohenriquep@outlook.com>
2024-02-20 22:28:08 -0300

    Aprendendo sobre Surface e Modifier

--- app/src/main/java/com/example/firstappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firstappkotlin/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.firstappkotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firstappkotlin.ui.theme.FirstAppKotlinTheme
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,10 +33,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Surface(color = Color.Magenta) {
+        Text(
             text = "Hello $name!",
-            modifier = modifier
-    )
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
